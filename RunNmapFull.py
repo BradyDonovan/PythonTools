db5d798d7d6eee799f4f7523164e9881699d6b70
BradyDonovan <bdonovan95@gmail.com>
2018-11-25 22:14:43 -0600

    Adding RunNmapFull.py

---
+++ RunNmapFull.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+if len(sys.argv) == 1:
+        print "No arguments specified."
+
+else:
+        targetIP = sys.argv[1]
+        print "Target: "+targetIP
+        yn = raw_input("Run full nmap scan on "+targetIP+"? Enter Y/N to continue: ")
+        if (yn == "yes" or yn == "y" or yn == "Y"):
+                print "Running full scan on "+targetIP
+                os.system('nmap -Pn -n -v -sT --host-timeout=30m -p- '+targetIP+' -oA '+targetIP+'_allports')
+        if (yn == "no" or yn == "n" or yn == "N"):
+                print "Scan cancelled. Quitting."
